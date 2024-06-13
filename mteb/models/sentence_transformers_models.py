963e7e218a263bcd6715c405b0c7fadebe4afc36
anpalmak2003 <73543260+anpalmak2003@users.noreply.github.com>
2024-06-13 22:48:14 +0300

    Update sentence_transformers_models.py

--- mteb/models/sentence_transformers_models.py
+++ mteb/models/sentence_transformers_models.py
@@ -74,17 +74,17 @@ paraphrase_multilingual_MiniLM_L12_v2 = ModelMeta(
 
 rubert_tiny2 = ModelMeta(
     name="cointegrated/rubert-tiny2",
-    languages=["rus_Cyrl","eng_Latn"],
+    languages=["rus_Cyrl"],
     open_source=True,
-    revision=15,  # can be any
+    revision="dad72b8f77c5eef6995dd3e4691b758ba56b90c3",  # can be any
     release_date="2021-10-28",
 )
 
 rubert_tiny = ModelMeta(
     name="cointegrated/rubert-tiny",
-    languages=["rus_Cyrl","eng_Latn"],
+    languages=["rus_Cyrl"],
     open_source=True,
-    revision=27,  # can be any
+    revision="5441c5ea8026d4f6d7505ec004845409f1259fb1",  # can be any
     release_date="2021-05-24",
 )
 
@@ -93,7 +93,7 @@ sbert_large_nlu_ru = ModelMeta(
     name="ai-forever/sbert_large_nlu_ru",
     languages=["rus_Cyrl"],
     open_source=True,
-    revision=20,  # can be any
+    revision="af977d5dfa46a3635e29bf0ef383f2df2a08d47a",  # can be any
     release_date="2020-11-20",
 )
 
@@ -101,6 +101,6 @@ sbert_large_mt_nlu_ru = ModelMeta(
     name="ai-forever/sbert_large_mt_nlu_ru",
     languages=["rus_Cyrl"],
     open_source=True,
-    revision=9,  # can be any
+    revision="05300876c2b83f46d3ddd422a7f17e45cf633bb0",  # can be any
     release_date="2021-05-18",
 )
